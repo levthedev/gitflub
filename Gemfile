75bc1e617002c786fe16cbe843b24b697b877b8b
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-20 15:57:34 -0600

    sqlite to pg

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'bootstrap-sass', '~> 3.3.3'
 gem 'omniauth-github'
 gem 'figaro'
 gem "octokit", "~> 3.0"
+gem "pg"
 
 group :development, :test do
   gem 'better_errors'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150720155302) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "nickname"
     t.string   "email"
