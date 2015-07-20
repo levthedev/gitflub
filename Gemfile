5b649aa55042366e17ccd7ac718b8b1bb17d826f
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-20 14:52:48 -0600

    add github gem and update view

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3.3'
 gem 'omniauth-github'
 gem 'figaro'
+gem "octokit", "~> 3.0"
 
 group :development, :test do
   gem 'better_errors'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['github_key'], ENV['github_secret']
 end
+
+Octokit.configure do |c|
+  c.login = ENV['github_login']
+  c.password = ENV['github_password']
+end
