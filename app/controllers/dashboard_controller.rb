4e085b805131cbd8ed2dce1ecaa47f7a048720f1
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-21 13:56:29 -0600

    fixed single user issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def show
+    @client = Octokit::Client.new(access_token: current_user.token)
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['github_key'], ENV['github_secret']
 end
 
-Octokit.configure do |c|
-  c.login = ENV['github_login']
-  c.password = ENV['github_password']
-end
+# Octokit.configure do |c|
+#   c.login = ENV['github_login']
+#   c.password = ENV['github_password']
+# end
