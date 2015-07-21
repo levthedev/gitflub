5404351d459aa2cb4f76b0ae3e06d27bf81577c3
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-21 13:56:58 -0600

    delete comment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['github_key'], ENV['github_secret']
 end
-
-# Octokit.configure do |c|
-#   c.login = ENV['github_login']
-#   c.password = ENV['github_password']
-# end
