7f5c833971cea26537b5c8c5c0bca8bf3b6f0227
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-23 00:00:06 -0600

    timeline feed and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,13 +18,6 @@ class User < ActiveRecord::Base
     chart
   end
 
-  def timeline
-    client   = Hurley::Client.new("http://github.com/")
-    body     = client.get("/").body
-    document = Nokogiri::HTML(body)
-    alerts   = document.css(".alert")
-  end
-
   def contribution_stats
     client          = Hurley::Client.new("http://github.com/")
     body            = client.get("#{nickname}").body
@@ -32,5 +25,16 @@ class User < ActiveRecord::Base
     contributions   = document.css(".contrib-number")
     c = contributions.to_s
     c.delete(',').gsub(/[^0-9]/, '/').split('/').reject(&:empty?)
+  end
+
+  def github_client
+    @client ||= Octokit::Client.new(access_token: token)
+  end
+
+  def following_feed
+    events = github_client.following.map do |user|
+      github_client.user_events(user.login)
+    end.flatten
+    events.sort_by { |event| event.created_at }.reverse
   end
 end
